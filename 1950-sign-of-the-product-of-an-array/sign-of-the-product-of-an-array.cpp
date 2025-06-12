d2c0ffe0e7e6d6323ba201b0e3472d6494ae10fa
Ishan Amrit Srivastava <98459119+IshanAmrit28@users.noreply.github.com>
2025-06-12 12:00:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (13.55%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int ans=1;
+        for(auto num:nums){
+            if(num<0){
+                ans*=-1;
+            }
+            else if(num==0){
+                return 0;
+            }
+        }
+        return ans;
+    }
+};
