3d3bf880ee8cfbb4dffa97b3f10622718f3270d1
Ishan Amrit Srivastava <ishansrivastava2805@gmail.com>
2026-06-13 12:45:25 +0530

    Time: 56 ms (27.19%) | Memory: 90.6 MB (74.10%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int>mp;
-        for(auto x:nums){
-            mp[x]++;
-        }
-        for(auto x:mp){
-            if(x.second>1){
+        // int n=nums.size();
+        // unordered_map<int,int>mp;
+        // for(auto x:nums){
+        //     mp[x]++;
+        // }
+        // for(auto x:mp){
+        //     if(x.second>1){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        unordered_set<int>st;
+        for(auto &x:nums){
+            if(st.count(x)>0){
                 return true;
             }
+            st.insert(x);
         }
         return false;
     }
